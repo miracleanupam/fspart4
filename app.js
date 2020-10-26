a1bef3e0e10d7de94c2a36d4f20c2a842554eb9c
Anupam Dahal <miracleanupam@gmail.com>
2020-10-26 20:56:01 +0545

    exercise-4.1,4.2; refactored the code so that creation and fetch of the blogs records are possible

---
+++ app.js
@@ -0,0 +1,77 @@
+// const http = require('http')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+// const mongoUrl = 'mongodb://localhost/bloglist'
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
+
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+// /////////////////////////////////////////////////////////////////////////////////
+
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('Connected to MongoDB');
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB', error.message);
+    });
+
+app.use(cors());
+// app.use(express.stattic('build'))
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app
---
+++ controllers/blogs.js
@@ -0,0 +1,49 @@
+const blogsRouter = require('express').Router()
+
+const { response } = require('express');
+const Blog = require('../models/blog');
+
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs);
+    })
+})
+
+blogsRouter.get('/:id', (req, res, nxt) => {
+    Blog.findById(req.params.id).then(note => {
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).end()
+        }
+    })
+        .catch(error => nxt(error));
+})
+
+blogsRouter.post('/', (req, res, nxt) => {
+    const blog = new Blog(req.body);
+
+    blog.save().then(savedBlog => {
+        res.json(savedBlog);
+    }).catch(error => nxt(error));
+})
+
+// blogsRouter.delete('/:id', (req, res, nxt) => {
+//     Blog.findByIdAndRemove(request.params.id).then(() => {
+//         response.status(204).end()
+//     }).catch(error => nxt(error));
+// })
+
+// blogsRouter.put('/:id', (req, res, nxt) => {
+//     const body = request.body;
+
+//     const blog = {
+//         content: body.content
+//     }
+
+//     Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedBlog => {
+//         response.json(updatedBlog);
+//     }).catch(error => nxt(error));
+// })
+
+module.exports = blogsRouter;
