ca91cfd35588d1f295ce13aa3162fcd1bcf063bf
Anupam Dahal <miracleanupam@gmail.com>
2020-10-28 16:56:19 +0545

    exercise 4.3,4.4; tests for dummy function and totalLikes function

---
+++ tests/list_helpers.test.js
@@ -0,0 +1,67 @@
+const listHelper = require('../utils/list_helper');
+
+test('dummy return one', () => {
+    const blogs = [];
+
+    const result = listHelper.dummy(blogs);
+
+    expect(result).toBe(1);
+})
+
+describe("total likes", () => {
+    test("of empty list is zero", () => {
+        expect(listHelper.totalLikes([])).toBe(0);
+    });
+
+    test("when list has only one blog equals the like of that", () => {
+        const likes = listHelper.totalLikes([{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.amazon.com',
+            likes: 5,
+            __v: 0
+        }]);
+
+        expect(likes).toBe(5);
+    });
+
+    test('of bigger list is calculated right', () => {
+        const likes = listHelper.totalLikes([
+            {
+                _id: '5a422aa71b54a676234d17f9',
+                title: 'Harry Potter: Philosopher\'s Stone',
+                author: 'JK Rowling',
+                url: 'http://www.amazon.com',
+                likes: 105,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d1000',
+                title: 'Harry Potter: Chamber of Secrets',
+                author: 'JK Rowling',
+                url: 'http://www.amazon.com',
+                likes: 205,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d1001',
+                title: 'Harry Potter: Prisioner of Azkaban',
+                author: 'JK Rowling',
+                url: 'http://www.amazon.com',
+                likes: 305,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d1002',
+                title: 'Harry Potter: Goblet of Fire',
+                author: 'JK Rowling',
+                url: 'http://www.amazon.com',
+                likes: 405,
+                __v: 0
+            }
+        ]);
+        const expectedValue = 105+205+305+405;
+        expect(likes).toBe(expectedValue);
+    })
+})
